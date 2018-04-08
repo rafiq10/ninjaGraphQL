124bef4f2b51f0f4a7fc0ac423b979b335895e8c
rafiq10 <rafiq10@gmail.com>
2018-04-08 13:26:55 +0200

    first react component

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import BookList from './components/BookList';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="main">
+          <h1>Ninja's reading list</h1>
+          <BookList />
+      </div>
+    );
+  }
+}
+
+export default App;
