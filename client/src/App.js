c940d45a5a1a8939b60e02dfcc156fedac10d0a9
rafiq10 <rafiq10@gmail.com>
2018-04-14 12:17:02 +0200

    apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
 
 import BookList from './components/BookList';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+});
 
 class App extends Component {
   render() {
     return (
+      <ApolloProvider client={client}>
       <div id="main">
           <h1>Ninja's reading list</h1>
           <BookList />
       </div>
+      </ApolloProvider>
     );
   }
 }
