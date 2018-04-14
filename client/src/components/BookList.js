106339251ac4397ab302691b95bad151eaaf2969
rafiq10 <rafiq10@gmail.com>
2018-04-14 13:19:59 +0200

    rendering data into react component

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -11,12 +11,28 @@ const getBooksQuery = gql`
     }
 `
 class BookList extends React.Component{
+    displayBooks(){
+        var data = this.props.data;
+        if(data.loading){
+            return(
+                <div>Loading books...</div>
+            )
+        }
+        else{
+            return data.books.map(book => {
+                return(
+                    <li key={book.id}>{book.name}</li>
+                )}
+            )
+        }
+    }
+
     render(){
         console.log(this.props);
         return(
             <div>
                 <ul id="book-list">
-                    <li>Book name</li>
+                    {this.displayBooks()}
                 </ul>
             </div>
         )
